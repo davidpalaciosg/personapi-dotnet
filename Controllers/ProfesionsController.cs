2792d21469ddd2fcde19d28e9ca7510c7c330b9d
David Enrique Palacios Garcia <paladavid@hotmail.com>
2023-04-27 12:55:10 -0500

    Profesions controller done as a Web Page

---
+++ Controllers/ProfesionsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using personapi_dotnet.Interface;
+using personapi_dotnet.Models.Entities;
+using personapi_dotnet.Repository;
+
+namespace personapi_dotnet.Controllers
+{
+    public class ProfesionsController : Controller
+    {
+        private readonly IProfesionRepository _context;
+
+        public ProfesionsController()
+        {
+            _context = new ProfesionRepository();
+        }
+
+        // GET: Profesions
+        public async Task<IActionResult> Index()
+        {
+            return View(_context.GetAll());
+        }
+
+        // GET: Profesions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                Console.WriteLine("Not id given");
+                return NotFound();
+            }
+
+            var profesion = _context.GetByID((int)id);
+            if (profesion == null)
+            {
+                Console.WriteLine("Profesion Not found");
+                return NotFound();
+            }
+
+            return View(profesion);
+        }
+
+        // GET: Profesions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profesions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nom,Des")] Profesion profesion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Insert(profesion);
+                _context.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(profesion);
+        }
+
+        // GET: Profesions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var profesion = _context.GetByID((int)id);
+            if (profesion == null)
+            {
+                return NotFound();
+            }
+            return View(profesion);
+        }
+
+        // POST: Profesions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nom,Des")] Profesion profesion)
+        {
+            if (id != profesion.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Update(profesion);
+                    _context.Save();
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(profesion);
+            }
+            return View(profesion);
+        }
+
+        // GET: Profesions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profesion = _context.GetByID((int)id);
+            if (profesion == null)
+            {
+                return NotFound();
+            }
+
+            return View(profesion);
+        }
+
+        // POST: Profesions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            _context.Delete((int)id);
+            _context.Save();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
